d5c595bf3f330faa509858f219499e1544963795
martinfj <martinfj@ryanair.com>
2023-07-15 01:24:31 +0200

    delete resource v4

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-#variable "subject" {
-#   type = string
-#   default = "tf-controller"
-#   description = "Subject to hello"
-#}
+variable "subject" {
+   type = string
+   default = "tf-controller"
+   description = "Subject to hello"
+}
 
-#output "hello_world" {
-#  value = "Auto sync, ${var.subject}!"
-#}
+output "hello_world" {
+  value = "Auto sync, ${var.subject}!"
+}
